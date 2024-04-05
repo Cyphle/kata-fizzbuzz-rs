48414ef5aecf9d930517e741fa0901a24e178603
Cyphle <cyril.phamle@gmail.com>
2024-04-05 13:57:22 +0200

    Step one: print something

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let suite = vec![1, 2, 3];
+
+    let converted = suite
+        .iter()
+        .map(|x|
+            if *x == 1 {
+                "Fizz"
+            } else {
+                "None"
+            }
+        )
+        .collect::<Vec<&str>>()
+        .join("\n");
+
+    println!("Hello, world! {}", converted);
 }
