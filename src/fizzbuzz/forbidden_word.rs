87fdfe5e838724ce60936bcc40092462941649d0
Cyril Pham-le <cyril.pham-le@stonal.com>
2024-05-23 13:51:39 +0200

    replace number tests

--- src/fizzbuzz/forbidden_word.rs
+++ src/fizzbuzz/forbidden_word.rs
@@ -1,9 +1,20 @@
-pub fn replace_number(number: u32) -> String {
+pub fn replace_number(number: u8) -> String {
     let forbidden = vec![3, 5];
 
     if forbidden.contains(&number) {
         "Fizz".to_string()
     } else {
         format!("{}", number)
+    }
+}
+
+#[cfg(test)]
+mod replace_number_tests {
+    use super::*;
+
+    #[test]
+    fn should_replace_number_by_fizz() {
+        let result = replace_number(2);
+        assert_eq!(result, "4");
     }
 }
