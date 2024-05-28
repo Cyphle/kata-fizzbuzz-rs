5d670232fa69de5743c2a1fc2a91267068d8c1cb
Cyril Pham-le <cyril.pham-le@stonal.com>
2024-05-28 13:13:20 +0200

    is dividable by

--- src/fizzbuzz/forbidden_word.rs
+++ src/fizzbuzz/forbidden_word.rs
@@ -1,20 +1,64 @@
 pub fn replace_number(number: u8) -> String {
     let forbidden = vec![3, 5];
 
-    if forbidden.contains(&number) {
+    if is_dividable_by(number, 3) {
         "Fizz".to_string()
+    } else if is_dividable_by(number, 5) {
+        "Buzz".to_string()
     } else {
         format!("{}", number)
     }
+}
+
+pub fn is_dividable_by(to_test: u8, divisor: u8) -> bool {
+    to_test % divisor == 0
 }
 
 #[cfg(test)]
 mod replace_number_tests {
     use super::*;
 
-    #[test]
-    fn should_replace_number_by_fizz() {
-        let result = replace_number(2);
-        assert_eq!(result, "4");
+    mod fizz {
+        use crate::replace_number;
+
+        #[test]
+        fn should_replace_number_by_fizz_when_three() {
+            let result = replace_number(3);
+            assert_eq!(result, "Fizz");
+        }
+
+        #[test]
+        fn should_not_replace_by_fizz_when_not_three() {
+            let result = replace_number(2);
+            assert_eq!(result, "2");
+        }
+
+        #[test]
+        fn should_replace_number_by_fizz_when_dividable_by_three() {
+            let result = replace_number(6);
+            assert_eq!(result, "Fizz");
+        }
+    }
+
+    mod dividable_by_three {
+        use crate::fizzbuzz::forbidden_word::is_dividable_by;
+
+        #[test]
+        fn should_return_true_when_three() {
+            let res = is_dividable_by(3, 3);
+            assert_eq!(res, true)
+        }
+
+        #[test]
+        fn should_return_true_when_six() {
+            let res = is_dividable_by(6, 3);
+            assert_eq!(res, true)
+        }
+
+        #[test]
+        fn should_return_false_when_not_dividable_by_three() {
+            let res = is_dividable_by(5, 3);
+            assert_eq!(res, false);
+        }
     }
 }
